812744e59f7d6d4331f4249d52fed3e33f62115b
Vinicius De Freitas Pereira <vde-frei@c1r1p1.42sp.org.br>
2023-07-11 11:29:30 -0300

    fix ex10 & add ft_iterative

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -1,20 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/10 22:35:34 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/07/11 11:11:30 by vde-frei         ###   ########.fr       */
+/*   Created: 2023/07/11 09:52:08 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/07/11 11:11:05 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_swap(int *a, int *b)
+int	ft_iterative_factorial(int nb)
 {
-	int	swp;
+	int	i;
+	int	factorial;
 
-	swp = *a;
-	*a = *b;
-	*b = swp;
+	i = 1;
+	factorial = 1;
+	if (nb < 0)
+		return (0);
+	while (i <= nb)
+		factorial = factorial * i++;
+	return (factorial);
+	return (0);
 }
