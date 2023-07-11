553eb7a2b92b69bcdee1f07cc9cf503109b05ed3
vde-frei <vde-frei@student.42sp.org.br>
2023-07-11 19:22:00 -0300

    argcv

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/11 19:15:36 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/07/11 19:20:29 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+int main (int argc, char *argv[])
+{
+	int	i;
+	int j;
+	(void)argc;
+
+	j = 1;
+	while (argv[j])
+	{
+		i = 0;
+		while (argv[j][i])
+		{
+			ft_putchar(argv[j][i]);
+			++i;
+		}
+		ft_putchar('\n');
+		++j;
+	}
+	return (0);
+
+}
