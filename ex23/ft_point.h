4c281770e6e85fca54ae4fa798b19170f5c5d330
vde-frei <vde-frei@student.42sp.org.br>
2023-07-12 20:50:09 -0300

    header ex23

---
+++ ex23/ft_point.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_point.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42sp.org.b      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 19:59:12 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/07/12 20:22:33 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_POINT_H
+# define FT_POINT_H
+
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}t_point;
+
+extern void	set_point(t_point *point);
+
+#endif //FT_POINT_H
