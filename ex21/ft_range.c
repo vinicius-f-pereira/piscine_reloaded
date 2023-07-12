77f8896898973a5162590cc6b9d644c18494c747
vde-frei <vde-frei@student.42sp.org.br>
2023-07-12 11:16:32 -0300

    fix norm ft_range

--- ex21/ft_range.c
+++ ex21/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 10:48:18 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/07/12 11:13:49 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/07/12 11:16:03 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	*ft_range(int min, int max)
 {
 	int	*numbers;
 	int	counter;
-	
+
 	if (min >= max)
 		return ((void *) 0);
 	numbers = (int *)malloc(sizeof(int) * (max));
@@ -24,16 +24,4 @@ int	*ft_range(int min, int max)
 	while (min != (max - 1))
 		numbers[counter++] = min++;
 	return (numbers);
-}
-#include <stdio.h>
-
-int	main(void)
-{
-	int	numbers = *ft_range(10, 30);
-
-	for(int i = 10; i < 30; i++)
-	{
-		printf("%d, ", numbers++);
-	}	
-	return (0);
 }
