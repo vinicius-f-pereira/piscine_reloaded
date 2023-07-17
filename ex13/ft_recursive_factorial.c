493a056ac19e8e53b5896e2efd994574377a78aa
vde-frei <vde-frei@student.42sp.org.br>
2023-07-17 18:06:14 -0300

    fix ex 	modified:   ex13/ft_recursive_factorial.c

--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -6,15 +6,16 @@
 /*   By: vde-frei <vde-frei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/11 11:30:32 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/07/11 11:45:17 by vde-frei         ###   ########.fr       */
+/*   Updated: 2023/07/17 16:20:44 by vde-frei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0)
+	if (nb < 0 || nb >= 13)
 		return (0);
-	if (nb == 0 || nb == 1)
+	if (nb == 0)
 		return (1);
 	return (nb * ft_recursive_factorial(nb - 1));
+	return (0);
 }
